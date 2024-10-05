8228b564890b32daa6164eaea9de8206b210411a
SahilTiwari-dev <sahiltiwari.contactme@gmail.com>
2024-10-05 18:22:53 +0530

    Added bg-color and a temporary logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bodyColor: "#0a192f",
+        textColor: "#CCD6F6",
+        specialText: "#64ffda",
+        headingColor: "#ccd6f6",
+        subHeadingColor: "#a8b2d1",
+        peragraphText: "#8892b0",
+      },
+    },
   },
   plugins: [],
-}
-
+};
